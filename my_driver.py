31e1926dec3a01ba48e2480cbf02297689704ae8
Arran Lyon <arran.lyon@hotmail.com>
2017-11-14 15:21:53 +0100

    Added comments

--- my_driver.py
+++ my_driver.py
@@ -5,17 +5,26 @@ import torch
 class MyDriver(Driver):
 
     def drive(self, carstate: State):
-        global current_index
-
         command = Command()
 
-        # Recieve Input Data
+        """
+        Collect Input Data.
+
+        Speed, Track Position, Angle on the track and the 19 values of the
+        Track Edges
+        """
+
         sensor_SPEED = carstate.speed_x # * 3600 / 1000  # Convert from MPS to KPH
         sensor_TRACK_POSITION = carstate.distance_from_center
         sensor_ANGLE_TO_TRACK_AXIS = carstate.angle
         sensor_TRACK_EDGES = carstate.distances_from_edge
 
-        # Automatic Transmission:
+        """
+        Automatic Transmission.
+
+        Automatically change gears depending on the engine revs
+        """
+
         if carstate.rpm > 8000:
             command.gear = carstate.gear + 1
         elif carstate.rpm < 2500:
@@ -24,14 +33,26 @@ class MyDriver(Driver):
         if not command.gear:
             command.gear = carstate.gear or 1
 
-        # Process Input
-        # print(sensor_TRACK_EDGES)
+        """
+        Process Inputs.
+
+        Feed the sensor data into the network to produce a Accelrator, Brake
+        and Steering command
+        """
+
+        accel = 1.0
+        brake = 0.0
+        steer = 0.0
 
 
-        # Apply Accelrator, Brake and Steering Commands
 
-        command.accelerator = 1.0
-        command.brake = 0.0
-        command.steering = 0.0
+
+        """
+        Apply Accelrator, Brake and Steering Commands.
+        """
+
+        command.accelerator = accel
+        command.brake = brake
+        command.steering = steer
 
         return command
