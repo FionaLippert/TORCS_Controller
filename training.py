035fc19de8b0a20b14a35f02c2b66c4633538a0d
Fiona Lippert <fiona@lipperta.de>
2017-11-19 12:15:59 +0100

    added the possibility to train based on a previously trained net (use trained paramenters instead of random initialization)

--- training.py
+++ training.py
@@ -10,22 +10,26 @@ STORAGE_PATH = "./trained_nn/net.pkl"
 train a neural net with the given training data (as .csv file)
 save the trained net at the given location
 
-run the script in the terminal in the following way
-python training.py ../train_data/aalgorg.csv ./trained_nn/my_net.pkl
+run the script in the terminal in the following way:
+python training.py ../train_data/aalgorg.csv ./trained_nn/my_net.pkl [./trained_nn/net.pkl]
 
-if no arguments are given, the default paths will be used
+    - if the third argument is given, the corresponding trained network is used as starting point for parameter training
+    - if no arguments are given, the default paths will be used
 """
 
 #
-if len(sys.argv)==3:
+if len(sys.argv)>=3:
 
-    path_to_training_data = sys.argv[1]
-    input_data, target_data = neuralNet.load_training_data(path_to_training_data)
-    print("Training data loaded from " + path_to_training_data)
+    input_data, target_data = neuralNet.load_training_data(sys.argv[1])
+    print("Training data loaded from " + sys.argv[1])
 
     storage_path = sys.argv[2]
-    neuralNet.train(input_data, target_data, storage_path)
-    print("Neural net trained and saved to " + storage_path)
+    if len(sys.argv)==4:
+        neuralNet.train(input_data, target_data, sys.argv[2], sys.argv[3])
+        print("Initial network state loaded from " + sys.argv[3])
+    else:
+        neuralNet.train(input_data, target_data, sys.argv[2])
+    print("Neural net trained and saved to " + sys.argv[2])
 
 else:
 
