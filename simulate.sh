900618c9aa50416aff55d19328e509d74246ac76
Fiona Lippert <fiona@lipperta.de>
2017-11-30 23:17:46 +0100

    EA stuff

--- simulate.sh
+++ simulate.sh
@@ -2,3 +2,8 @@
 
 args=("$@")
 torcs -r ~/Documents/MScComputationalScience/CI/torcs-server/torcs-client/practice.xml & ./start.sh
+
+#gnome-terminal -e "torcs -r ~/Documents/MScComputationalScience/CI/torcs-server/torcs-client/practice.xml"
+#gnome-terminal -e "./start.sh -p 3001"
+
+#gnome-terminal -e "./start.sh -p 3001" && gnome-terminal -e "./start.sh -p 3002"
